39e8d83a8c89079faf8296fe2dbc938b56b2a2f9
pohoreckim <mikolaj.pohorecki@gmail.com>
2022-06-19 21:42:35 +0200

    Code rework

--- mocmaes.py
+++ mocmaes.py
@@ -3,17 +3,18 @@ import matplotlib.pyplot as plt
 import math
 import sys
 
-mu = 100	
+popSize = 100	
 maxGenerations = 50	
 
-pTarget = pow(5 + math.sqrt(0.5), -1) 
-dDamping = 1 + mu/2 			
-cSuccRateParam = pTarget/(2 + pTarget)	
-cCumulTimeParam = 2/(2 + mu)		
-cCov = 2/(pow(mu,2) + 6)		
+lambd = 1
+dDamping = 1 + popSize/(2 * lambd) 	
+pTarget = pow(5 + math.sqrt(lambd / 2.0), -1)
+cP = (pTarget * lambd)/(2 + (pTarget * lambd))	
+cC = 2/(2 + popSize)		
+cCov = 2/(pow(popSize,2) + 6)		
 pThresh = 0.44	
 
-MAX = pow(10,6)
+INF = pow(10,10)
 
 def computeCrowdingDistances(individuals, n, dim):
 
@@ -27,16 +28,18 @@ def computeCrowdingDistances(individuals, n, dim):
 		perm = dim_i.argsort()
 		dim_i = dim_i[perm]
 
-		crowdingDistances[perm[0]] = MAX
-		crowdingDistances[perm[n-1]] = MAX
+		crowdingDistances[perm[0]] = INF
+		crowdingDistances[perm[n-1]] = INF
 
 		for j in range(2, n-1):
 			crowdingDistances[perm[j]] += (dim_i[i+1] - dim_i[i-1]) / (dim_i[n-1] - dim_i[0])
 
 	return crowdingDistances
 
-def selectBest(individuals, mu):
-
+def selectBest(individuals, popSize):
+	
+	nextGen = []
+	nextGenCount = 0			
 	n = len(individuals)			
 	dim = individuals[0].fitness.size	
 
@@ -50,16 +53,13 @@ def selectBest(individuals, mu):
 				nonDominationRanks[j] += 1
 
 	perm = nonDominationRanks.argsort()
-	nonDominationRanks = nonDominationRanks[perm]	
 	individuals = np.array(individuals)[perm]
-
-	nextGenCount = 0			# keeps count of the number of individuals in the next generation
-	nextGen = []				# stores the best mu individuals and builds the next generation
+	nonDominationRanks = nonDominationRanks[perm]	
 	
-	for level in range(2 * mu):		# for each possible level of nondominance: sort all individuals of..
-		tempIndividuals = []		# ..that level according to their crowding distance
+	for level in range(2 * popSize):		
+		tempIndividuals = []
 		tempCrowdingDistances = []
-		while(nonDominationRanks[nextGenCount] == level and nextGenCount < mu):
+		while(nonDominationRanks[nextGenCount] == level and nextGenCount < popSize):
 			tempIndividuals.append(individuals[nextGenCount])
 			tempCrowdingDistances.append(crowdingDistances[nextGenCount])
 			nextGenCount += 1
@@ -91,7 +91,22 @@ class Individual:
 
 		self.inputDim = self.x.size
 		self.outputDim = self.fitness.size
+
+	def updateStepSize(self):
+		if self.succ:
+			self.pSucc = (1 - cP)*self.pSucc + cP
+		else:
+			self.pSucc = (1 - cP)*self.pSucc
+
+		self.sigma = self.sigma * math.exp((self.pSucc - pTarget) / (dDamping*(1 - pTarget)))
 		
+	def updateCovariance(self):
+		if self.pSucc < pThresh:
+			self.pEvol = (1 - cC) * self.pEvol + math.sqrt(cC * (2 - cC)) * self.step
+			self.C = (1 - cCov) * self.C + cCov * (np.transpose(self.pEvol) * self.pEvol)
+		else:
+			self.pEvol = (1 - cC) * self.pEvol
+			self.C = (1 - cCov) * self.C + cCov * (np.transpose(self.pEvol) * self.pEvol + cC * (2 - cC)*self.C)
 
 	def dominates(self, other):
 		result = False
@@ -101,22 +116,6 @@ class Individual:
 			if self.fitness[i] < other.fitness[i]:		
 				result = True
 		return result
-
-	def updateStepSize(self):
-		if self.succ:
-			self.pSucc = (1 - cSuccRateParam)*self.pSucc + cSuccRateParam
-		else:
-			self.pSucc = (1 - cSuccRateParam)*self.pSucc
-
-		self.sigma = self.sigma * math.exp((self.pSucc - pTarget) / (dDamping*(1 - pTarget)))
-		
-	def updateCovariance(self):
-		if self.pSucc < pThresh:
-			self.pEvol = (1 - cCumulTimeParam) * self.pEvol + math.sqrt(cCumulTimeParam * (2 - cCumulTimeParam)) * self.step
-			self.C = (1 - cCov) * self.C + cCov * (np.transpose(self.pEvol) * self.pEvol)
-		else:
-			self.pEvol = (1 - cCumulTimeParam)*self.pEvol
-			self.C = (1 - cCov)*self.C + cCov*(np.transpose(self.pEvol) * self.pEvol + cCumulTimeParam * (2 - cCumulTimeParam)*self.C)
 
 	def mutate(self):
 		newx = np.random.multivariate_normal(self.x, pow(self.sigma,2) * self.C)
@@ -136,7 +135,7 @@ class MOCMAES:
 			inputDim = x0.size
 
 			currentPop = []	
-			for i in range(mu):
+			for i in range(popSize):
 				xi = np.random.rand(inputDim) * 4 * initialSigma - 2 * initialSigma
 				Ci = np.identity(inputDim)
 				currentPop.append(Individual(xi, pTarget, initialSigma, 0, Ci, func))
@@ -144,10 +143,10 @@ class MOCMAES:
 			for g in range(0, maxGenerations):
 
 				Q = []
-				for k in range(mu):
+				for k in range(popSize):
 					Q.append(currentPop[k].mutate())
 
-				for k in range(mu):
+				for k in range(popSize):
 					currentPop[k].updateStepSize()
 					Q[k].updateStepSize()
 
@@ -155,7 +154,7 @@ class MOCMAES:
 
 					Q.append(currentPop[k])
 
-				currentPop = selectBest(Q, mu)
+				currentPop = selectBest(Q, popSize)
 
 			return currentPop[0].x
 
