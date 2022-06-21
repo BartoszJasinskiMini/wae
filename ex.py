46aef2da846a533499fc2731dd402a0c2711f901
bartosz <bjasinski@student.mini.pw.edu.pl>
2022-06-21 08:54:50 +0200

    EGO push

--- ex.py
+++ ex.py
@@ -46,11 +46,16 @@ import numpy as np  # for median, zeros, random, asarray
 import cocoex  # experimentation module
 import mocmaes
 from pyDOE import lhs
-from smt.applications import EGO
+# from smt.applications import EGO
+from smt.sampling_methods import LHS
 import matplotlib.pyplot as plt
-# from EGO import EGO
+from EGO import EGO
+from mipego import ParallelBO, ContinuousSpace, OrdinalSpace, NominalSpace, RandomForest, BO
+from mipego import OptimizerPipeline, BO, ContinuousSpace, Solution, GaussianProcess
+from mipego.optimizer import OnePlusOne_Cholesky_CMA
 from kriging import kriging
 from true_function import true_function
+from mipego.GaussianProcess.trend import constant_trend
 
 try: import cocopp  # post-processing module
 except: pass
@@ -161,30 +166,164 @@ for batch_counter, problem in enumerate(suite):  # this loop may take hours or d
         elif fmin is mocmaes.MOCMAES:
             output = mocmaes.MOCMAES.run(problem, propose_x0())
         elif fmin is EGO:
-            X = lhs(2, samples=propose_x0().size)
-            y = np.zeros((propose_x0().size, 1))
+            #
+            # # sampling plan
+            # criterion = 'EI'  # 'EI' or 'SBO' or 'LCB'
+            #
+            # # number of points in the initial DOE
+            # ndoe = 2  # (at least ndim+1)
+            #
+            # # number of iterations with EGO
+            # n_iter = 5
+            #
+            # # Build the initial DOE, add the random_state option to have the reproducibility of the LHS points
+            #
+            # xlimits = np.array([[0, 2]])
+            # sampling = LHS(xlimits=xlimits, random_state=1)
+            # xdoe = sampling(ndoe)
+            #
+            # # EGO call
+            # ego = EGO(n_iter=n_iter, criterion=criterion, n_doe=ndoe, xlimits=xlimits)
+            #
+            # x_opt, y_opt, ind_best, x_data, y_data = ego.optimize(fun=problem)
+            #
+            # output = x_opt
 
-            for i in range(k):
-                y[i] = true_function(X[i], 1)
+            # k = 2
+            # n = 5 * 2
+            #
+            # X = lhs(2, samples=propose_x0().size)
+            # y = np.zeros((propose_x0().size, 2))
+            #
+            # for i in range(k):
+            #     y[i] = problem(X[i])
+            #
+            # kr = kriging(k, X, y)
+            #
+            # kr.train()
+            #
+            #
+            # E = EGO(kr)
+            # MinExpImp = 1e14
+            # infill = 0
+            #
+            # while infill < 3 * n:
+            #     Xnew, EI = E.next_infill()
+            #     Ynew = problem(Xnew)
+            #     kr.X = np.vstack((kr.X, Xnew))
+            #     kr.y = np.vstack((kr.y, Ynew.T))
+            #     infill = infill + 1
+            #
+            #     kr.train()
+            #     output = Xnew
 
-            kr = kriging(k, X, y)
+            # seed = 666
+            # np.random.seed(seed)
+            # dim_r = 2  # dimension of the real values
+            #
+            #
+            # # def obj_fun(x):
+            # #     x_r = np.array([x['continuous_%d' % i] for i in range(dim_r)])
+            # #     x_i = x['ordinal']
+            # #     x_d = x['nominal']
+            # #     _ = 0 if x_d == 'OK' else 1
+            # #     return np.sum(x_r ** 2) + abs(x_i - 10) / 123. + _ * 2
+            #
+            #
+            # # Continuous variables can be specified as follows:
+            # # a 2-D variable in [-5, 5]^2
+            # # for 2 variables, the naming scheme is continuous0, continuous1
+            # C = ContinuousSpace([-5, 5], var_name='continuous') * dim_r
+            #
+            # # Integer (ordinal) variables can be specified as follows:
+            # # The domain of integer variables can be given as with continuous ones
+            # # var_name is optional
+            # I = OrdinalSpace([5, 15], var_name='ordinal')
+            #
+            # # Discrete (nominal) variables can be specified as follows:
+            # # No lb, ub... a list of categories instead
+            # N = NominalSpace(['OK', 'A', 'B', 'C', 'D', 'E', 'F', 'G'], var_name='nominal')
+            #
+            # # The whole search space can be constructed:
+            # search_space = C + I + N
+            #
+            # # Bayesian optimization also uses a Surrogate model
+            # # For mixed variable type, the random forest is typically used
+            # model = RandomForest(levels=search_space.levels)
+            #
+            # # opt = ParallelBO(
+            # #     search_space=search_space,
+            # #     obj_fun=problem,
+            # #     model=model,
+            # #     max_FEs=50,
+            # #     DoE_size=3,  # the initial DoE size
+            # #     eval_type='dict',
+            # #     acquisition_fun='MGFI',
+            # #     acquisition_par={'t': 2},
+            # #     n_job=3,  # number of processes
+            # #     n_point=3,  # number of the candidate solution proposed in each iteration
+            # #     verbose=True  # turn this off, if you prefer no output
+            # # )
+            #
+            # bo = BO(
+            #     search_space=search_space,
+            #     obj_fun=problem,
+            #     model=model,
+            #     eval_type='list',
+            #     DoE_size=10,
+            #     n_point=1,
+            #     acquisition_fun='EI',
+            #     verbose=True,
+            #     minimize=True
+            # )
+            #
+            # xopt, fopt, stop_dict = bo.run()
+            #
+            # print('xopt: {}'.format(xopt))
+            # print('fopt: {}'.format(fopt))
+            # print('stop criteria: {}'.format(stop_dict))
+            # output = xopt
 
-            kr.train()
+            np.random.seed(42)
 
+            dim = 2
+            max_FEs = 100
+            obj_fun = problem
+            lb, ub = -600, 600
 
-            E = EGO(kr)
-            MinExpImp = 1e14
-            infill = 0
+            search_space = ContinuousSpace([lb, ub]) * dim
+            mean = constant_trend(dim, beta=None)
 
-            while infill < 3 * n:
-                Xnew, EI = E.next_infill()
-                Ynew = true_function(Xnew, 1)
-                kr.X = np.vstack((kr.X, Xnew))
-                kr.y = np.vstack((kr.y, Ynew))
-                infill = infill + 1
+            # autocorrelation parameters of GPR
+            thetaL = 1e-10 * (ub - lb) * np.ones(dim)
+            thetaU = 10 * (ub - lb) * np.ones(dim)
+            theta0 = np.random.rand(dim) * (thetaU - thetaL) + thetaL
 
-                kr.train()
-                output = EI
+            model = GaussianProcess.GaussianProcess(
+                corr='squared_exponential',
+                theta0=theta0, thetaL=thetaL, thetaU=thetaU,
+                nugget=1e-6, noise_estim=False,
+                optimizer='BFGS', wait_iter=5, random_start=5 * dim,
+                eval_budget=100 * dim, mean=mean
+            )
+
+            bo = BO(
+                search_space=search_space,
+                obj_fun=problem,
+                model=model,
+                eval_type='list',
+                DoE_size=10,
+                n_point=1,
+                acquisition_fun='EI',
+                verbose=True,
+                minimize=True
+            )
+            xopt, fopt, stop_dict = bo.run()
+
+            print('xopt: {}'.format(xopt))
+            print('fopt: {}'.format(fopt))
+            print('stop criteria: {}'.format(stop_dict))
+            output = xopt
 
         elif fmin is scipy.optimize.fmin_slsqp:
             output = fmin(problem, propose_x0(), iter=int(evalsleft() / problem.dimension + 1),  # very approximate way to respect budget
--- kriging_test.py
+++ kriging_test.py
@@ -6,33 +6,86 @@ from kriging import kriging
 from true_function import true_function
 from ga import ga
 
+#
+# k = 2
+# n = 5*2
+#
+# X = lhs(k, samples=n)
+# y = np.zeros((n, 1))
+#
+# for i in range(k):
+#     y[i] = true_function(X[i], 1)
+#
+# kr = kriging(k, X, y)
+#
+# kr.train()
+#
+# kr.plot_2d()
+#
+# E = EGO(kr)
+# MinExpImp = 1e14
+# infill = 0
+#
+# while abs(MinExpImp) > 1e-3 and infill < 3*n:
+#     Xnew, EI = E.next_infill()
+#     Ynew = true_function(Xnew, 1)
+#     kr.X = np.vstack((kr.X, Xnew))
+#     kr.y = np.vstack((kr.y, Ynew))
+#     infill = infill + 1
+#
+#     kr.train()
+#     # kr.plot_2d()
+#
+import numpy as np
+from mipego import ParallelBO, ContinuousSpace, OrdinalSpace, NominalSpace, RandomForest
 
-k = 2
-n = 5*2
+seed = 666
+np.random.seed(seed)
+dim_r = 2  # dimension of the real values
 
-X = lhs(k, samples=n)
-y = np.zeros((n, 1))
+def obj_fun(x):
+    x_r = np.array([x['continuous_%d'%i] for i in range(dim_r)])
+    x_i = x['ordinal']
+    x_d = x['nominal']
+    _ = 0 if x_d == 'OK' else 1
+    return np.sum(x_r ** 2) + abs(x_i - 10) / 123. + _ * 2
 
-for i in range(k):
-    y[i] = true_function(X[i], 1)
+# Continuous variables can be specified as follows:
+# a 2-D variable in [-5, 5]^2
+# for 2 variables, the naming scheme is continuous0, continuous1
+C = ContinuousSpace([-5, 5], var_name='continuous') * dim_r
 
-kr = kriging(k, X, y)
+# Integer (ordinal) variables can be specified as follows:
+# The domain of integer variables can be given as with continuous ones
+# var_name is optional
+I = OrdinalSpace([5, 15], var_name='ordinal')
 
-kr.train()
+# Discrete (nominal) variables can be specified as follows:
+# No lb, ub... a list of categories instead
+N = NominalSpace(['OK', 'A', 'B', 'C', 'D', 'E', 'F', 'G'], var_name='nominal')
 
-kr.plot_2d()
+# The whole search space can be constructed:
+search_space = C + I + N
 
-E = EGO(kr)
-MinExpImp = 1e14
-infill = 0
+# Bayesian optimization also uses a Surrogate model
+# For mixed variable type, the random forest is typically used
+model = RandomForest(levels=search_space.levels)
 
-while abs(MinExpImp) > 1e-3 and infill < 3*n:
-    Xnew, EI = E.next_infill()
-    Ynew = true_function(Xnew, 1)
-    kr.X = np.vstack((kr.X, Xnew))
-    kr.y = np.vstack((kr.y, Ynew))
-    infill = infill + 1
+opt = ParallelBO(
+    search_space=search_space,
+    obj_fun=obj_fun,
+    model=model,
+    max_FEs=50,
+    DoE_size=3,    # the initial DoE size
+    eval_type='dict',
+    acquisition_fun='MGFI',
+    acquisition_par={'t' : 2},
+    n_job=3,       # number of processes
+    n_point=3,     # number of the candidate solution proposed in each iteration
+    verbose=True   # turn this off, if you prefer no output
+)
+xopt, fopt, stop_dict = opt.run()
 
-    kr.train()
-    # kr.plot_2d()
-
+print('xopt: {}'.format(xopt))
+print('fopt: {}'.format(fopt))
+print('stop criteria: {}'.format(stop_dict))
